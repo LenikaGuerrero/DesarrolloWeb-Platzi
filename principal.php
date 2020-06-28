10121ffbb6a8e99282e7377bd135020f00e1968d
Lenika Guerrero <lenikaguerrero@gmail.com>
2020-06-27 17:55:22 -0700

    Desarrollo Web Online

---
+++ principal.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head lang="es">
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<div>
+		<h1>
+			Buscador de
+			<br>
+			<b>Escuelas en Línea</b>
+		</h1>	
+	</div>
+	<div>
+		
+	</div>
+</body>
+</html>
